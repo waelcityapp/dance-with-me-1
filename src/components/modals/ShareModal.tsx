7804e97186bdf2592691ac54026470c2129df487
waelcityapp <waelvts@gmail.com>
2026-09-07 20:33:21 +0300

    Bust stale social share preview cache

--- src/components/modals/ShareModal.tsx
+++ src/components/modals/ShareModal.tsx
@@ -22,7 +22,8 @@ export const ShareModal: React.FC<ShareModalProps> = ({ event, onClose }) => {
 
   const isArabic = lang === 'ar';
   const shareTitle = stripLegacyRepoLinks(isArabic ? event.titleAr : event.titleEn);
-  const shareUrl = `https://cityeve.online/e/${event.id}`;
+  // Versioned share URL prevents WhatsApp/Facebook from reusing an old preview cache.
+  const shareUrl = `https://cityeve.online/e/${event.id}?v=2`;
 
   const descSnippet = stripLegacyRepoLinks(isArabic ? event.descriptionAr : event.descriptionEn || '')
     .replace(/[\r\n]+/g, ' ')
