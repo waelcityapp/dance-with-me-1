ce4fc414076e6971175bb476360b6623d758416b
waelcityapp <waelvts@gmail.com>
2026-09-14 19:29:43 +0300

    Fix JSX guard around admin-only views control

--- src/components/events/CreateEventPage.tsx
+++ src/components/events/CreateEventPage.tsx
@@ -2370,10 +2370,9 @@ export const CreateEventPage: React.FC<CreateEventPageProps> = ({ onComplete, on
                   </div>
                 </div>
               </div>
+          )}
             </div>
           </div>
-
-          )}
 
           {/* SECTION 6: Subscription Plan & Dynamic Calculation */}
           <div className="space-y-4 border-t border-amber-200/60 dark:border-white/10 pt-6">
